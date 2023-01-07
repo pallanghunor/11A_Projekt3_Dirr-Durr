620ed99e50ca1f5a17fe95354b1665d9866e7c6d
pallanghunor <pallang.hunor@students.jedlik.eu>
2023-01-08 00:08:27 +0100

    map done

---
+++ game.js
@@ -0,0 +1,193 @@
+const game = document.getElementById("game");
+const player1 = document.getElementById("player1");
+const player1X = 0;
+const player1Y = 0;
+const player2 = document.getElementById("player2");
+const player2X = 14;
+const player2Y = 14;
+let squares;
+
+// Függvények
+function generateMap() {
+  // Generate squares with coordinates
+  for (let i = 0; i <= 14; i++) {
+    for (let j = 0; j <= 14; j++) {
+      let square = document.createElement("div");
+      square.classList.add("square");
+      square.setAttribute("data-x", i);
+      square.setAttribute("data-y", j);
+      square.style.top = (i * 40) + "px";
+      square.style.left = (j * 40) + "px";
+      square.textContent = `(${i}, ${j})`;
+      game.appendChild(square);
+    }
+  }
+  squares = document.querySelectorAll(".square");
+}
+
+function generateWall() {
+  // Generate walls
+  for (let i = 1; i < 15; i++) {
+    for (let j = 1; j < 15; j++) {
+      const square = squares[i * 15 + j];
+      if (!square) continue; // skip if square is null or undefined
+      if (i % 2 === 1 && j % 2 === 1) {
+        square.classList.add("wall"); // add "wall" class
+      }
+    }
+  }
+}
+
+function removeSand(playerX, playerY) {
+  // Don't generate sand next to player
+  for (let i = playerX - 1; i <= playerX + 1; i++) {
+    for (let j = playerY - 1; j <= playerY + 1; j++) {
+      const square = squares[i * 15 + j];
+      if (!square) continue; // skip if square is null or undefined
+      square.classList.remove("sand");
+    }
+  }
+}
+
+function generateSand() {
+  for (let i = 0; i < squares.length; i++) {
+    if (Math.random() < 0.8 && !squares[i].classList.contains("wall")) {
+      squares[i].classList.add("sand");
+    }
+  }
+}
+
+function movePlayer(player, direction) {
+  // Get the current position of the player
+  let playerTop = player.offsetTop;
+  let playerLeft = player.offsetLeft;
+
+  // Update the position based on the direction
+  if (direction === "up") {
+    playerTop -= 40;
+  } else if (direction === "down") {
+    playerTop += 40;
+  } else if (direction === "left") {
+    playerLeft -= 40;
+  } else if (direction === "right") {
+    playerLeft += 40;
+  }
+
+  // Check if the new position of the player would collide with a wall or go out of bounds
+  let collidesWithWall = false;
+  let walls = document.querySelectorAll(".wall");
+  let sands = document.querySelectorAll(".sand");
+  walls.forEach(function (wall) {
+    if (playerLeft + player.offsetWidth > wall.offsetLeft &&
+      playerLeft < wall.offsetLeft + wall.offsetWidth &&
+      playerTop + player.offsetHeight > wall.offsetTop &&
+      playerTop < wall.offsetTop + wall.offsetHeight) {
+      console.log('Collides with wall!');
+      collidesWithWall = true;
+    }
+  });
+  sands.forEach(function (sand) {
+    if (playerLeft + player.offsetWidth > sand.offsetLeft &&
+      playerLeft < sand.offsetLeft + sand.offsetWidth &&
+      playerTop + player.offsetHeight > sand.offsetTop &&
+      playerTop < sand.offsetTop + sand.offsetHeight) {
+      console.log('Collides with wall!');
+      collidesWithWall = true;
+    }
+  });
+
+  if (playerLeft < 0 || playerLeft + player.offsetWidth > game.offsetWidth ||
+    playerTop < 0 || playerTop + player.offsetHeight > game.offsetHeight) {
+    collidesWithWall = true;
+  }
+
+  // Only update the player's position if there is no collision
+  if (!collidesWithWall) {
+    player.style.top = playerTop + "px";
+    player.style.left = playerLeft + "px";
+  }
+}
+
+function placeBomb(player){
+  let playerTop = player.offsetTop;
+  let playerLeft = player.offsetLeft;
+
+  let bomb = document.createElement('div');
+  bomb.classList.add('bomb');
+  bomb.style.top = playerTop + 'px';
+  bomb.style.left = playerLeft + 'px';
+  bomb.dataset.x = Math.floor(playerTop / 40);
+  bomb.dataset.y = Math.floor(playerLeft / 40);
+  game.appendChild(bomb);
+
+  // After 3 seconds, explode the bomb
+  setTimeout(function() {
+    explodeBomb(bomb);
+  }, 3000);
+}
+
+function explodeBomb(bomb) {
+  bomb.remove();
+  let explosionRange = 1;
+
+  // Get the position of the bomb
+  let bombX = parseInt(bomb.dataset.x);
+  let bombY = parseInt(bomb.dataset.y);
+
+  
+
+  // Get the coordinates of the squares next to the bomb
+  let squaresToRemove = [
+    [bombX, bombY - explosionRange], // square above the bomb
+    [bombX, bombY + explosionRange], // square below the bomb
+    [bombX - explosionRange, bombY], // square to the left of the bomb
+    [bombX + explosionRange, bombY] // square to the right of the bomb
+  ];
+
+  
+
+  // Remove the sand from the squares next to the bomb
+  squaresToRemove.forEach(function(coords) {
+    let square = document.querySelector(`[data-x='${coords[0]}'][data-y='${coords[1]}']`);
+    console.log(square);
+    if (square && square.classList.contains('sand')) {
+      square.classList.remove('sand');
+    }
+  });
+}
+
+document.addEventListener("keydown", function (event) {
+  if (event.key == "w") {
+    movePlayer(player1, "up");
+  } else if (event.key == "a") {
+    movePlayer(player1, "left");
+  } else if (event.key == "s") {
+    movePlayer(player1, "down");
+  } else if (event.key == "d") {
+    movePlayer(player1, "right");
+  } else if (event.key == "ArrowUp") {
+    movePlayer(player2, "up");
+  } else if (event.key == "ArrowLeft") {
+    movePlayer(player2, "left");
+  } else if (event.key == "ArrowDown") {
+    movePlayer(player2, "down");
+  } else if (event.key == "ArrowRight") {
+    movePlayer(player2, "right");
+  } else if (event.code == 'Space'){
+    placeBomb(player1);
+  } else if (event.key == "j"){
+    placeBomb(player2);
+  }
+
+});
+
+
+// Főprogram
+generateMap();
+generateWall();
+generateSand();
+removeSand(player1X, player1Y);
+removeSand(player2X, player2Y);
+
+
+
