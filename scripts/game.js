27dc790c4b6741995baf454c5aa7d4c91cefc3a1
pallanghunor <pallang.hunor@students.jedlik.eu>
2023-02-09 17:28:14 +0100

    player movement animation added

--- scripts/game.js
+++ scripts/game.js
@@ -1,9 +1,15 @@
 class Player {
-  constructor(player, x, y, image, speed) {
-    this.player = player;
-    this.x = x * 40;
-    this.y = y * 40;
+  constructor({player, x, y, image, speed, sprites}) {
     this.image = image;
+    this.sprites = sprites;
+    this.frameWidth = 0;
+    this.playerWidth = this.image.width / 4;
+    this.playerHeight = this.image.height;
+    this.player = player;
+    this.x = x;
+    this.y = y;
+    this.moving = false;
+    this.animationSpeed = 10;
     this.speed = speed;
     this.bombImage = new Image();
     this.bombImage.src = './img/dirr_durr_bomba1.png';
@@ -12,35 +18,55 @@ class Player {
 
 
   moveUp() {
-    if (isInsideMap(this.x, this.y - this.speed) && !isColliding(this.x, this.y - this.speed)) {
+    this.moving = true;
+    this.image = this.sprites.up;
+    if (isInsideMap(this.x, this.y - this.speed, this.playerWidth, this.playerHeight) && !isColliding(this.x, this.y - this.speed, this.playerWidth, this.playerHeight)) {
       this.y -= this.speed;
     }
   }
 
   moveLeft() {
-    if (isInsideMap(this.x - this.speed, this.y) && !isColliding(this.x - this.speed, this.y)) {
+    this.moving = true;
+    this.image = this.sprites.left;
+    if (isInsideMap(this.x - this.speed, this.y, this.playerWidth, this.playerHeight) && !isColliding(this.x - this.speed, this.y, this.playerWidth, this.playerHeight)) {
       this.x -= this.speed;
     }
   }
 
   moveDown() {
-    if (isInsideMap(this.x, this.y + this.speed) && !isColliding(this.x, this.y + this.speed)) {
+    this.moving = true;
+    this.image = this.sprites.down;
+    if (isInsideMap(this.x, this.y + this.speed, this.playerWidth, this.playerHeight) && !isColliding(this.x, this.y + this.speed, this.playerWidth, this.playerHeight)) {
       this.y += this.speed;
     }
   }
 
   moveRight() {
-    if (isInsideMap(this.x + this.speed, this.y) && !isColliding(this.x + this.speed, this.y)) {
+    this.moving = true;
+    this.image = this.sprites.right;
+    if (isInsideMap(this.x + this.speed, this.y, this.playerWidth, this.playerHeight) && !isColliding(this.x + this.speed, this.y, this.playerWidth, this.playerHeight)) {
       this.x += this.speed;
     }
   }
 
   placeBomb() {
-    this.bomb.placeBomb(this.x, this.y);
+    this.bomb.placeBomb(this.x, this.y, this.playerHeight, this.playerWidth);
   }
 
   draw() {
-    ctx.drawImage(this.image, this.x + mapXStart, this.y);
+    if (this.moving) {
+      if (frames % this.animationSpeed == 0) {
+        if (this.frameWidth < 3) {
+          this.frameWidth++;
+        } else{
+          this.frameWidth = 0;
+        }
+      }
+    }
+    ctx.drawImage(this.image, this.frameWidth * this.playerWidth, 0, this.playerWidth, this.playerHeight,
+                  this.x + mapXStart, this.y + mapYStart - this.playerHeight + 40,
+                  this.playerWidth, this.playerHeight);
+
   }
 
   update() {
@@ -60,6 +86,9 @@ class Player {
       if (keys.space.pressed && !this.bomb.bombPlaced) {
         this.placeBomb();
       }
+      if (!keys.w.pressed && !keys.a.pressed && !keys.s.pressed && !keys.d.pressed) {
+        this.moving = false;
+      }
     } else if (this.player == 2) {
       if (keys.arrowUp.pressed) {
         this.moveUp();
@@ -75,6 +104,9 @@ class Player {
       }
       if (keys.e.pressed && !this.bomb.bombPlaced) {
         this.placeBomb();
+      }
+      if (!keys.arrowUp.pressed && !keys.arrowLeft.pressed && !keys.arrowDown.pressed && !keys.arrowRight.pressed) {
+        this.moving = false;
       }
     }
     this.draw();
@@ -94,7 +126,7 @@ class Bomb {
     this.bombImage.src = './img/dirr_durr_bomba1.png';
   }
 
-  placeBomb(playerX, playerY) {
+  placeBomb(playerX, playerY, playerHeight, playerWidth) {
     if (!this.bombPlaced) {
       this.bombPlaced = true;
       this.x = playerX;
@@ -105,13 +137,14 @@ class Bomb {
       let block2Y = 0;
       block1X = Math.floor(this.x / 40);
       block1Y = Math.floor(this.y / 40);
-      block2X = Math.floor((this.x + 39) / 40);
-      block2Y = Math.floor((this.y + 39) / 40);
+      block2X = Math.floor((this.x + playerWidth - 1) / 40);
+      block2Y = Math.floor((this.y + playerHeight - (playerHeight - 40) - 1) / 40);
+      // console.log(playerX, playerY);
       // console.log(`player: ${this.x}:${this.y}`);
       // console.log(`block1: ${block1X}:${block1Y}`);
       // console.log(`block2: ${block2X}:${block2Y}`);
       if (block1Y == block2Y && block1X != block2X) {
-        if (((block1X + 1) * 40) - this.x > (this.x + 40) - (((block2X + 1) * 40) - 40)) {
+        if (((block1X + 1) * 40) - this.x > (this.x + playerWidth) - (((block2X + 1) * 40) - 40)) {
           this.bombX = block1X;
           this.bombY = block1Y;
         } else {
@@ -119,7 +152,7 @@ class Bomb {
           this.bombY = block2Y;
         }
       } else if (block1X == block2X && block1Y != block2Y) {
-        if (((block1Y + 1) * 40) - this.y > (this.y + 40) - (((block2Y + 1) * 40) - 40)) {
+        if (((block1Y + 1) * 40) - this.y > (this.y + playerHeight) - (((block2Y + 1) * 40) - 40)) {
           this.bombX = block1X;
           this.bombY = block1Y;
         } else {
@@ -131,7 +164,7 @@ class Bomb {
         this.bombY = block1Y;
       }
       // console.log(this.bombX, this.bombY);
-      ctx.drawImage(this.bombImage, this.bombX * 40 + mapXStart, this.bombY * 40);
+      ctx.drawImage(this.bombImage, this.bombX * 40 + mapXStart, this.bombY * 40 + mapYStart);
       setTimeout(() => {
         this.bombExplode();
       }, this.bombTimer);
@@ -198,7 +231,7 @@ class Bomb {
   }
 
   draw(){
-    ctx.drawImage(this.bombImage, (this.bombX * 40) + mapXStart, this.bombY * 40);
+    ctx.drawImage(this.bombImage, (this.bombX * 40) + mapXStart, this.bombY * 40 + mapYStart);
   }
 
   update(){
@@ -212,23 +245,72 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth - 50;
 canvas.height = window.innerHeight - 50;
+let frames = 0;
 
 let mapXStart = (canvas.width / 2) - 300;
-let mapYStart = 0;
+let mapYStart = 20;
 let map = Array(15).fill("0").map(() => Array(15).fill("0"));
 let mapWidth = 600;
 let mapHeight = 600;
 
+let player1SelectedCharachter = "01";
+let player1DownImage = new Image();
+player1DownImage.src = `./img/characters/${player1SelectedCharachter}/playerDown.png`;
 
-let player1Image = new Image();
-player1Image.src = "./img/character.png";
-let player1 = new Player(1, 0, 0, player1Image, 2);
+let player1UpImage = new Image();
+player1UpImage.src = `./img/characters/${player1SelectedCharachter}/playerUp.png`;
 
-let player2Image = new Image();
-player2Image.src = "./img/character.png"
-let player2 = new Player(2, 14, 14, player2Image, 2);
+let player1LeftImage = new Image();
+player1LeftImage.src = `./img/characters/${player1SelectedCharachter}/playerLeft.png`;
 
-window.onload = init;
+let player1RightImage = new Image();
+player1RightImage.src = `./img/characters/${player1SelectedCharachter}/playerRight.png`;
+console.log(player1RightImage);
+
+
+let player1 = new Player({
+  player: 1,
+  x: 0, 
+  y: 0,
+  image: player1DownImage, 
+  speed: 2,
+  sprites: {
+    up: player1UpImage,
+    left: player1LeftImage,
+    right: player1RightImage,
+    down: player1DownImage
+  }
+});
+
+let player2SelectedCharachter = "01";
+
+let player2DownImage = new Image();
+player2DownImage.src = `./img/characters/${player2SelectedCharachter}/playerDown.png`;
+
+let player2UpImage = new Image();
+player2UpImage.src = `./img/characters/${player2SelectedCharachter}/playerUp.png`;
+
+let player2LeftImage = new Image();
+player2LeftImage.src = `./img/characters/${player2SelectedCharachter}/playerLeft.png`;
+
+let player2RightImage = new Image();
+player2RightImage.src = `./img/characters/${player2SelectedCharachter}/playerRight.png`;
+
+let player2 = new Player({
+  player: 2,
+  x: 0, 
+  y: 0,
+  image: player2DownImage, 
+  speed: 2,
+  sprites: {
+    up: player2UpImage,
+    left: player2LeftImage,
+    right: player2RightImage,
+    down: player2DownImage
+  }
+});
+
+
 const keys = {
   w: {
     pressed: false
@@ -261,23 +343,36 @@ const keys = {
     pressed: false
   }
 }
+Loading();
+
+function Loading(){
+  window.onload;
+  player1DownImage.onload;
+  player1UpImage.onload;
+  player1DownImage.onload;
+  player1LeftImage.onload;
+
+  player2DownImage.onload;
+  player2UpImage.onload;
+  player2LeftImage.onload;
+  player2RightImage.onload;
+  init();
+}
 
 function init() {
   generateWall();
   generateSand();
   console.log(map);
+  gameLoop();
 }
 
 function gameLoop() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawMap();
   player1.update();
-  player2.update();
   player1.bomb.update();
-  player2.bomb.update();
-  requestAnimationFrame(gameLoop);
-}
-requestAnimationFrame(gameLoop);
+  frames++;
+} setInterval(gameLoop , 1000/ 60)
 
 function generateWall() {
   for (let i = 1; i < 15; i++) {
@@ -318,35 +413,35 @@ function drawMap() {
       switch (map[i][j]) {
         case 'sand':
           ctx.fillStyle = "yellow";
-          ctx.fillRect(j * 40 + mapXStart, i * 40, 40, 40);
+          ctx.fillRect(j * 40 + mapXStart, i * 40 + mapYStart, 40, 40);
           break;
         case 'wall':
           ctx.fillStyle = "black";
-          ctx.fillRect(j * 40 + mapXStart, i * 40, 40, 40);
+          ctx.fillRect(j * 40 + mapXStart, i * 40 + mapYStart, 40, 40);
           break;
         case '0':
           ctx.fillStyle = "beige";
-          ctx.fillRect(j * 40 + mapXStart, i * 40, 40, 40);
+          ctx.fillRect(j * 40 + mapXStart, i * 40 + mapYStart, 40, 40);
           break;
       }
     }
   }
 }
 
-function isInsideMap(playerX, playerY) {
+function isInsideMap(playerX, playerY, playerWidth, playerHeight) {
   const x = playerX + mapXStart;
-  const y = playerY;
-  if (x < mapXStart || x + 40 > mapXStart + mapWidth || y < mapYStart || y + 40 > mapYStart + mapHeight) {
+  const y = playerY + mapYStart;
+  if (x < mapXStart || x + playerWidth > mapXStart + mapWidth || y < mapYStart || y + playerHeight > mapYStart + mapHeight) {
     return false;
   }
   return true;
 }
 
-function isColliding(playerX, playerY) {
+function isColliding(playerX, playerY, playerWidth, playerHeight) {
   const xStart = Math.floor(playerX / 40);
-  const yStart = Math.floor(playerY / 40);
-  const xEnd = Math.floor((playerX + 39) / 40);
-  const yEnd = Math.floor((playerY + 39) / 40);
+  const yStart = Math.floor((playerY + (playerHeight - 40)) / 40);
+  const xEnd = Math.floor((playerX + playerWidth - 1) / 40);
+  const yEnd = Math.floor((playerY + playerHeight - (playerHeight - 40) - 1) / 40);
   if (map[yStart][xStart] == "wall" || map[yEnd][xEnd] == "wall" || map[yEnd][xStart] == "wall" || map[yStart][xEnd] == "wall") {
     return true;
   }
